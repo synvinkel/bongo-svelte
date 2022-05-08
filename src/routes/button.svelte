3074f2814ee968fcd8b2e2596742b5aad3655170
Johnnie Hård <johnnie@synvinkel.org>
2022-05-08 08:20:40 +0200

    Demo buttons.

--- src/routes/button.svelte
+++ src/routes/button.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
 	import Button from '$lib/Button.svelte';
+
+	function demoClick() {
+		alert('Clicked it!');
+	}
 </script>
 
-<Button size="small">Small</Button>
+<div class="wrapper">
+	<Button size="small" on:click={demoClick}>Small</Button>
 
-<Button size="medium">medium</Button>
+	<Button size="medium" on:click={demoClick}>medium</Button>
 
-<Button size="large">large</Button>
+	<Button size="large" on:click={demoClick}>large</Button>
+
+	<Button size="large" on:click={demoClick} disabled>Disabled</Button>
+
+	<Button type="contained" on:click={demoClick}>Contained</Button>
+
+	<Button type="outlined" on:click={demoClick}>Outlined</Button>
+
+	<Button type="outlined" loading on:click={demoClick}>Loading</Button>
+
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+
+		padding: 40px;
+	}
+</style>
